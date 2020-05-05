ac677f35863ba7b7af5e5d0a14c71fc7f9cf9601
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-05-05 15:27:44 +0700

    Slicing and Skip

---
+++ Slicing_Skip.py
@@ -0,0 +1,11 @@
+# list slicing
+lst = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# Slicing and skip 2 elements
+print(lst[0:len(lst):2])
+print(lst[::2])
+
+# string/sequence slicing
+name = "My name is Liam!"
+#slicing and skip 2 elements
+print(name[0:len(name):2])
+print(name[::2])
